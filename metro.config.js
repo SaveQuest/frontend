d3426cfbc11b6b5167a52b9fa83d362e800424bd
blluv <biluv@protonmail.com>
2024-09-18 04:51:10 +0900

    svg를 임포트 가능하도록

--- metro.config.js
+++ metro.config.js
@@ -5,8 +5,13 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 config.resolver = {
-  sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs', 'mjs'],
-  assetExts: ['glb', 'gltf', 'png', 'jpg','ttf','otf'],
+  sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs', 'mjs', 'svg'],
+  assetExts: ['glb', 'gltf', 'png', 'jpg','ttf', 'otf'],
 };
+
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
+}
 
 module.exports = config;
